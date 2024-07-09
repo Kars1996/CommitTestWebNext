6e9c37cbcd68d2403ac61d4798cfcc9368c9f2ed
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-07-09 15:09:38 +0100

    fix: Dumb Oversight

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,42 +8,40 @@ Contact Kars for any enquieries
 type footerProp = {
     commitHash: any;
     commitMessage: any;
-}
+};
 
-export default function({ commitHash, commitMessage }: footerProp) {
+export default function ({ commitHash, commitMessage }: footerProp) {
     let hash;
     let message;
     if (!commitHash) {
         hash = "DEVELOPMENT";
         message = "DEVELOPMENT";
-      } else {
+    } else {
         hash = commitHash.slice(0, 7);
         message = commitMessage.slice(0, 18);
-    
-        if (commitMessage.length > 7) {
-          message += "...";
-        }
-      }
-    return(
-        <>
 
-<footer>
-                <div class="px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
-                  <div class="flex flex-col items-baseline space-y-6">
-                    <div class="mx-auto">
-                      <a href="/" class="mx-auto text-lg text-center">
-                        ⨀
-                      </a>
+        if (commitMessage.length > 7) {
+            message += "...";
+        }
+    }
+    return (
+        <>
+            <footer>
+                <div className="px-8 py-12 mx-auto md:px-12 lg:px-32 max-w-7xl">
+                    <div className="flex flex-col items-baseline space-y-6">
+                        <div className="mx-auto">
+                            <a href="/" className="mx-auto text-lg text-center">
+                                ⨀
+                            </a>
+                        </div>
+                        <div className="mx-auto">
+                            <span className="mx-auto mt-2 text-sm font-medium text-gray-500">
+                                Running version{hash} - {message}
+                            </span>
+                        </div>
                     </div>
-                    <div class="mx-auto">
-                      <span class="mx-auto mt-2 text-sm font-medium text-gray-500">
-                        Running version{hash} - {message}
-                      </span>
-                    </div>
-                  </div>
                 </div>
-              </footer>
-            
+            </footer>
         </>
-    )
-} 
+    );
+}
